f4f9d1508ab3edabd3a91518cf665f483a0d03d1
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2024-09-09 23:19:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        
+        // we will be using lambda function n n stable_partition() function 
+        auto it=stable_partition(arr.begin(), arr.end(),[](int n){
+            return n==0;
+        });
+        
+        stable_partition(it, arr.end(),[](int n){
+        return n==1;
+    });
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> a;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+        Solution ob;
+        ob.sort012(a);
+
+        int n = a.size();
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
