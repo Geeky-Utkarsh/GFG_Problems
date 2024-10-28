b7edabbebb0e5a4bea9cf35cc1bbc6b97cc62fd9
Geeky_utkarsh <48992400+Geeky-Utkarsh@users.noreply.github.com>
2024-10-28 13:31:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the Sum of Last N nodes of the Linked List/find-the-sum-of-last-n-nodes-of-the-linked-list.cpp
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    /*Structure of the node of the linled list is as
+
+    struct Node {
+        int data;
+        struct Node* next;
+
+        Node(int x){
+            data = x;
+            next = NULL;
+        }
+    };
+    */
+    // your task is to complete this function
+    // function should return sum of last n nodes
+    int sumOfLastN_Nodes(struct Node* head, int n) {
+        // Code here
+        vector<int>nodes;
+        
+        Node*tmp=head;
+        while(tmp){
+            nodes.push_back(tmp->data);
+            tmp=tmp->next;
+        }
+        
+        int sz=nodes.size();
+        int count=0, sum=0;
+        
+        for(int i=sz-1; i>=0; i--){
+            if(count>=n){
+             break;
+            }
+            
+            sum+=nodes[i];
+            count++;
+            
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n;
+        cin >> n;
+        cin.ignore();
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        Solution ob;
+        cout << ob.sumOfLastN_Nodes(head, n) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
